3b3b3df07d0a4769869abb3e5b4908521f3f38f1
Ali ghasemi <ag1166090@gmail.com>
2025-05-28 14:57:12 +0330

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/sharedpreferences/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/sharedpreferences/ExampleInstrumentedTest.java
@@ -13,13 +13,12 @@ import static org.junit.Assert.*;
 /**
  * Instrumented test, which will execute on an Android device.
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+ * @see <a href="http://d.android.com/tools/test
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
-        // Context of the app under test.
+    
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.sharedpreferences", appContext.getPackageName());
     }
